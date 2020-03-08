f66ccac8e3ebb7516d8d0eb61594498c3f790c5c
Anastasiia Kulachok <kulachokn@gmail.com>
2020-03-08 18:56:07 +0200

    Updates

---
+++ precache-manifest.4feca18948b976ec9707133eac4d1209.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ec896389af3ba55732e5129c2c49c8",
+    "url": "/homeTask_3.2/index.html"
+  },
+  {
+    "revision": "10321abd3c0c5f0f4f8c",
+    "url": "/homeTask_3.2/static/css/2.83670195.chunk.css"
+  },
+  {
+    "revision": "7dca0800138ff339e07e",
+    "url": "/homeTask_3.2/static/css/main.d7374cd1.chunk.css"
+  },
+  {
+    "revision": "10321abd3c0c5f0f4f8c",
+    "url": "/homeTask_3.2/static/js/2.4a9e57ec.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/homeTask_3.2/static/js/2.4a9e57ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dca0800138ff339e07e",
+    "url": "/homeTask_3.2/static/js/main.7e715c52.chunk.js"
+  },
+  {
+    "revision": "3b14b0e564bc98e31326",
+    "url": "/homeTask_3.2/static/js/runtime-main.02573930.js"
+  }
+]);
